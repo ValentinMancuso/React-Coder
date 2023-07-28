522e56974ece1e715c5b5f55ef9da2e5f41aed93
ValentinMancuso <valen.mancuso.m@gmail.com>
2023-07-28 18:13:37 -0300

    EntregaFinalMancuso

--- src/components/pages/checkout/CheckoutContainer.jsx
+++ src/components/pages/checkout/CheckoutContainer.jsx
@@ -9,6 +9,8 @@ import {
   doc,
 } from "firebase/firestore";
 import { Link } from "react-router-dom";
+import "../../pages/ItemDetail/ItemDetail.css";
+import { Button } from "@mui/material";
 
 const CheckoutContainer = () => {
   const [data, setData] = useState({ nombre: "", telefono: "", email: "" });
@@ -37,46 +39,92 @@ const CheckoutContainer = () => {
         stock: element.stock - element.cantidad,
       });
     });
-    clearCart()
+    clearCart();
   };
   const capturarInfo = (e) => {
     setData({ ...data, [e.target.name]: e.target.value });
-
   };
 
   return (
     <div>
-      <div>Checkout</div>
       {orderId ? (
-        <div>
-          <h3>Felicitaciones por su compra</h3>
-          <h4>Su numero de seguimiento es {orderId}</h4>
-          <button>
-            <Link to={"/"}>Seguir Comprando</Link>
-          </button>
+        <div className="page1">
+          <div
+            style={{
+              height: "500px",
+              display: "flex",
+              alignContent: "center",
+              flexWrap: "wrap",
+              flexDirection: "column",
+              justifyContent: "space-around",
+              alignItems: "center",
+            }}
+          >
+            <h1
+              style={{
+                width: "400px",
+                height: "30px",
+                marginBottom: "15px",
+                marginTop: "10px",
+              }}
+            >
+              Felicitaciones por su compra
+            </h1>
+            <h4>Su numero de seguimiento es {orderId}</h4>
+            <Button sx={{ textDecoration: "none" }} variant="contained">
+              <Link style={{ textDecoration: "none", color:"white"}} to={"/"}>
+                Seguir Comprando
+              </Link>
+            </Button>
+          </div>
         </div>
       ) : (
-        <form onSubmit={enviarCompra}>
-          <input
-            type="text"
-            name="nombre"
-            placeholder="_Ingrese su Nombre"
-            onChange={capturarInfo}
-          />
-          <input
-            type="text"
-            name="telefono"
-            placeholder="Ingrese su telefono"
-            onChange={capturarInfo}
-          />
-          <input
-            type="text"
-            name="email"
-            placeholder="Ingrese su email"
-            onChange={capturarInfo}
-          />
-          <button type="submit">Comprar</button>
-        </form>
+        <div className="page1">
+          <div>
+            <form
+              className="container1"
+              style={{
+                height: "500px",
+                flexDirection: "column",
+                alignContent: "space-around",
+                justifyContent: "center",
+                flexWrap: " wrap",
+              }}
+              onSubmit={enviarCompra}
+            >
+              <h1>Ingrese sus datos</h1>
+              <input
+                type="text"
+                name="nombre"
+                placeholder="Ingrese su Nombre"
+                onChange={capturarInfo}
+                style={{
+                  width: "400px",
+                  height: "30px",
+                  marginBottom: "15px",
+                  marginTop: "10px",
+                }}
+              />
+              <input
+                type="text"
+                name="telefono"
+                placeholder="Ingrese su telefono"
+                onChange={capturarInfo}
+                style={{ width: "400px", height: "30px", marginBottom: "15px" }}
+              />
+              <input
+                type="text"
+                name="email"
+                placeholder="Ingrese su email"
+                onChange={capturarInfo}
+                style={{ width: "400px", height: "30px", marginBottom: "15px" }}
+              />
+              <Button variant="contained" type="submit">
+                Comprar
+              </Button>
+            </form>
+          </div>
+        </div>
       )}
     </div>
   );
